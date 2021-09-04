344bb8ec77b5c154e963e7209735f5216efc3445
Sapan Patibandha <patibandha@hotmail.com>
2021-09-04 20:29:01 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BestWayToManageString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
